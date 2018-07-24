a713661cdac42458713cebb3136ef3d684c1a848
Philipp Kalytta (Endlesscrafter) <git@kalytta.net>
2018-07-24 21:16:44 +0200

    Site Footer and main nav ausgelagert

--- static/body.php
+++ static/body.php
@@ -5,57 +5,7 @@
 <script src="https://www.highcharts.com/media/com_demo/js/highslide.config.js" charset="utf-8"></script>
 <link rel="stylesheet" type="text/css" href="https://www.highcharts.com/media/com_demo/css/highslide.css"/>
 <wrapper class="d-flex flex-column">
-    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-        <a class="navbar-brand mr-auto mr-lg-0" href="#"><i class="fas fa-id-card"></i></a>
-        <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Notifications</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Profile</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Switch account</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown"
-                       aria-haspopup="true" aria-expanded="false">Settings</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown01">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-            <a href="#">
-                <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt=""
-                     class="ml-4 mt-1 rounded float-left">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col align-self-end">
-                            <span class="text-light">@Username</span>
-                        </div>
-                        <div class="w-100"></div>
-                        <div class="col align-self-end" style="margin-top: -0.5em;">
-                            <small class="text-light">Role</small>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-    </nav>
+    <?php include("site-nav-main.php") ?>
 
     <div class="nav-scroller box-shadow navbar-sub">
         <nav class="nav nav-underline">
@@ -612,12 +562,7 @@
             </main>
         </div>
     </div>
-
-    <footer class="footer">
-        <div class="container">
-            <span class="text-muted">Place sticky footer content here.</span>
-        </div>
-    </footer>
+    <?php include("site-footer.php") ?>
 </wrapper>
 <?php include("footer.php") ?>
 </body>
