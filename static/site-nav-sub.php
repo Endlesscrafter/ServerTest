fbc4fe2fb038f7855465228af7fa27c4dbc12ce9
Philipp Kalytta (Endlesscrafter) <git@kalytta.net>
2018-07-24 21:20:03 +0200

    Moved everything out of body

---
+++ static/site-nav-sub.php
@@ -0,0 +1,19 @@
+<div class="nav-scroller box-shadow navbar-sub">
+    <nav class="nav nav-underline">
+        <a class="nav-link active" href="#">Dashboard</a>
+        <a class="nav-link" href="#">
+            Friends
+            <span id="friendscounter" class="badge badge-pill badge-light align-text-bottom">27</span>
+        </a>
+        <a class="nav-link" href="#">
+            Messages
+            <span id="messagecounter" class="badge badge-pill badge-light align-text-bottom">155</span>
+        </a>
+        <a class="nav-link" href="#">Suggestions</a>
+        <a class="nav-link" href="#">Explore</a>
+        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Link</a>
+    </nav>
+</div>
