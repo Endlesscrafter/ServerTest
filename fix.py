f5fbc49bcb7fa6b41c8a36429bce19e626227812
Philipp Kalytta (Endlesscrafter) <git@kalytta.net>
2018-07-26 14:18:20 +0200

    changed log

--- fix.py
+++ fix.py
@@ -25,7 +25,7 @@ logging.debug('Definiere Daten')
 data = {"state": "OPEN"}
 
 with open('rolladen.json', 'w') as objectfile:
-    logging.info('Status wird überschrieben: ' + json.dump(dict))
+    logging.info('Status wird überschrieben: %s', data)
     json.dump(data, objectfile)
     #Tatsächliches Hochfahren
     logging.debug('Beginne Hochfahren...')
