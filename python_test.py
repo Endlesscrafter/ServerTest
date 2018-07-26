2037375b049c830281fea0ef403abc1f24549532
Philipp Kalytta (Endlesscrafter) <git@kalytta.net>
2018-07-26 12:17:08 +0200

    Blink

--- python_test.py
+++ python_test.py
@@ -1,5 +1,15 @@
-f = open("test.txt","w+")
-for i in range(10):
-     f.write("This is line %d\r\n" % (i+1))
-f.close()
-print("Hallo Welt")
+from gpiozero import LED
+from time import sleep
+
+red = LED(17)
+
+red.on()
+sleep(1)
+red.off()
+sleep(1)
+
+#f = open("test.txt","w+")
+#for i in range(10):
+#     f.write("This is line %d\r\n" % (i+1))
+#f.close()
+#print("Hallo Welt")
