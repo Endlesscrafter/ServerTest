95dac462826f613341e57b706550ae74efe1d736
Philipp Kalytta (Endlesscrafter) <git@kalytta.net>
2018-07-26 14:11:12 +0200

    Added Logging

--- fix.py
+++ fix.py
@@ -1,6 +1,7 @@
 from gpiozero import LED
 from time import sleep
 import json
+import logging
 
 #Dieses Programm ignoriert die Werte in der JSON Datei und fährt die Rolladen nach oben, schreibt dann ein OPEN in die JSON-Datei.
 #Es wird auf JSON als Austauschsprache gesetzt, so kann dann auch in der Webseite der Status angezeigt werden, falls benötigt.
@@ -11,6 +12,9 @@ import json
 #LIGHT (Kleiner Spalt lässt Licht rein),
 #HALF (Hälfte verdeckt)
 
+logging.basicConfig(filename='rolladen.log',level=logging.DEBUG)
+logging.debug('Start fix.py')
+
 #Definiere Ports
 up = LED(17)
 down = LED(22)
@@ -19,8 +23,13 @@ down = LED(22)
 data = {"state": "OPEN"}
 
 with open('rolladen.json', 'w') as objectfile:
+    logging.info('Status wird überschrieben: ' + data)
     json.dump(data, objectfile)
     #Tatsächliches Hochfahren
+    logging.debug('Beginne Hochfahren...')
     up.on()
     sleep(5)
+    logging.debug('Ende Hochfahren...')
     up.off()
+
+logging.debug('Ende fix.py')
--- rauf.py
+++ rauf.py
@@ -1,6 +1,7 @@
 from gpiozero import LED
 from time import sleep
 import json
+import logging
 
 #Programm zu kompletten Hochfahren der Rolladen. Dabei kann ruhig etwas länger hochgefahren werden, denn ein Relais stoppt bei Endanschlag.
 #Es wird auf JSON als Austauschsprache gesetzt, so kann dann auch in der Webseite der Status angezeigt werden, falls benötigt.
@@ -10,6 +11,14 @@ import json
 #SHUT (Komplett geschlossen),
 #LIGHT (Kleiner Spalt lässt Licht rein),
 #HALF (Hälfte verdeckt)
+
+logging.basicConfig(filename='rolladen.log',level=logging.INFO)
+logging.debug('Start fix.py')
+
+#Definiere Ports
+up = LED(17)
+down = LED(22)
+#I2C freilassen für etwaigen Arduino für Temperatur etc
 
 with open("rolladen.json","r") as objectfile:
 	data = json.load(objectfile)
@@ -21,3 +30,5 @@ if data['state'] != "OPEN":
     with open('rolladen.json', 'w') as objectfile:
         json.dump(data, objectfile)
     #Tatsächliches Hochfahren
+
+logging.debug('Ende rauf.py')
