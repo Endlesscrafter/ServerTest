99fab343718d9a3263e8cec1edd2cc5e7ff2b972
Philipp Kalytta (Endlesscrafter) <git@kalytta.net>
2018-09-21 20:02:38 +0200

    New Role

--- static/site/content/site-content-main.php
+++ static/site/content/site-content-main.php
@@ -95,7 +95,7 @@
 
     <!--Rolladen POSTs-->
     <div class="my-3 p-3 bg-white rounded box-shadow">
-        row my-3 p-3 bg-white rounded box-shadow
+
         <div class="row">
 
             <form action="steuerung.php" method="post" class="m-3">
@@ -281,7 +281,7 @@
                  class="mr-2 rounded">
             <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
                 <div class="d-flex justify-content-between align-items-center w-100">
-                    <strong class="text-gray-dark">Full Name <span class="badge badge-primary badge-admin">Admin</span></strong>
+                    <strong class="text-gray-dark">Full Name <span class="badge badge-primary badge-admin">Admin <i class="fas fa-crown"></i></span></strong>
                     <a href="#">Follow</a>
                 </div>
                 <span class="d-block">@username</span>
@@ -293,7 +293,19 @@
             <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
                 <div class="d-flex justify-content-between align-items-center w-100">
                     <strong class="text-gray-dark">Full Name <span
-                                class="badge badge-primary badge-moderator">Moderator</span></strong>
+                                class="badge badge-primary badge-moderator">Moderator <i class="fas fa-star"></i></span></strong>
+                    <a href="#">Follow</a>
+                </div>
+                <span class="d-block">@username</span>
+            </div>
+        </div>
+        <div class="media text-muted pt-3">
+            <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt=""
+                 class="mr-2 rounded">
+            <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                <div class="d-flex justify-content-between align-items-center w-100">
+                    <strong class="text-gray-dark">Full Name <span
+                                class="badge badge-primary badge-protector">Protector <i class="fas fa-shield-alt"></i></span></strong>
                     <a href="#">Follow</a>
                 </div>
                 <span class="d-block">@username</span>
