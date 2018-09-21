cfd9fd9df906d6c23cf56ecf5247a7d912e43758
Philipp Kalytta (Endlesscrafter) <git@kalytta.net>
2018-09-21 20:28:28 +0200

    Earth and Air

--- static/site/content/site-content-main.php
+++ static/site/content/site-content-main.php
@@ -345,6 +345,7 @@
                 <div class="d-flex justify-content-between align-items-center w-100">
                     <strong class="text-gray-dark">Full Name
                         <span class="badge badge-primary badge-fire">Fire <i class="fab fa-hotjar"></i></span>
+                        <span class="badge badge-primary badge-air">Air <i class="fas fa-feather-alt"></i></span>
                         <span class="badge badge-primary badge-supporter">Supporter <i
                                     class="fas fa-user-tie"></i></span></strong>
                     <a href="#">Follow</a>
@@ -360,6 +361,8 @@
                     <strong class="text-gray-dark">Full Name
                         <span class="badge badge-primary badge-ice">Ice <i
                                     class="fas fa-snowflake"></i></span>
+                        <span class="badge badge-primary badge-earth">Earth <i
+                                    class="fas fa-tree"></i></span>
                         <span class="badge badge-primary badge-user">User <i
                                     class="fas fa-user"></i></span></strong>
                     <a href="#">Follow</a>
